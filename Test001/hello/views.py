db59b68980385d59d9a760d1f2ad249a7c7a81fe
MD Siyam Bhuiyan <siyam35-702@diu.edu.bd>
2025-05-02 20:03:21 +0600

    created newyear app

--- Test001/hello/views.py
+++ Test001/hello/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, World")
+    return render(request,"hello/index.html")
 
 def siyam(request):
     return HttpResponse("Hello, Siyam!")
@@ -10,4 +10,4 @@ def another(request):
     return HttpResponse("Show another route")
 
 def greet(request,name):
-    return HttpResponse(f"hello, {name.capitalize()}")
+    return render(request,"hello/greet.html", {"name": name.capitalize()})
