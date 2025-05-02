c9bdcd164cc7cbf37e2eb9e0385708fc55aaaf6c
MD Siyam Bhuiyan <siyam35-702@diu.edu.bd>
2025-05-02 20:19:10 +0600

    updated New year app and created tasks app

--- Test001/newyear/views.py
+++ Test001/newyear/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 import datetime
-# Create your views here.
+
 def index(request):
     now = datetime.datetime.now()
+    new_year = datetime.datetime(year=now.year + 1, month=1, day=1)
+    countdown = new_year - now
+
+    is_new_year = now.month == 1 and now.day == 1
+
     return render(request, "newyear/index.html", {
-        "newyear": now.month == 1 and now.day == 1
+        "newyear": is_new_year,
+        "new_year_datetime": new_year.strftime('%Y-%m-%d %H:%M:%S')
     })
